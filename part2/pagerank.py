171776958a2fd6a70aa630f7f1d62d3731603e0f
Devansh Goenka <dgoenka@wisc.edu>
2022-09-23 16:02:06 -0600

    Clean dataset

--- part2/pagerank.py
+++ part2/pagerank.py
@@ -1,8 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark import StorageLevel
 from operator import add
-
-
-NUMBER_NODES = 685230
 
 # Create Spark context
 spark = SparkSession.builder.appName("PageRank").getOrCreate()
@@ -10,13 +8,19 @@ spark = SparkSession.builder.appName("PageRank").getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
 
 # Load RDD into Spark
-rdd = spark.sparkContext.textFile("hdfs://10.10.1.1:9000/part2/datasets/web-BerkStan.txt")
+#rdd = spark.sparkContext.textFile("hdfs://10.10.1.1:9000/part2/datasets/web-BerkStan.txt")
 
-# Cleaning up the commented lines
-rdd = rdd.filter(lambda line: '#' not in line)
+rdd = spark.sparkContext.textFile("hdfs://10.10.1.1:9000/part2/datasets/enwiki-pages-articles/link-enwiki-*")
+
+# Cleaning up the data
+rdd = rdd.\
+    filter(lambda line: '#' not in line).\
+    filter(lambda line: 'Category' not in line).\
+    filter(lambda line: 'Image' not in line).\
+    filter(lambda line: 'User' not in line)
 
 # Splitting tab separated lines into two columns
-rdd = rdd.map(lambda line: line.split('\t'))
+rdd = rdd.map(lambda line: line.split('\t', 1))
 
 # Peek the RDD
 #rdd.toDF(["From", "To"]).show()
@@ -29,6 +33,7 @@ neighbours = rdd.groupByKey().mapValues(list)
 # Create the rank map
 ranks = neighbours.mapValues(lambda x: 1.0)
 #ranks.toDF(["Node", "Rank"]).show()
+
 for _ in range(10):
     ranks = ranks.\
     join(neighbours).\
@@ -37,6 +42,5 @@ for _ in range(10):
     map(lambda row: (row[0], 0.15 + 0.85*row[1]))
 
 ranks.toDF(["Node", "Rank"]).show(50)
-
 
 spark.stop()
