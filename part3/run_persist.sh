c57d008d1c629ce598d60befa350ea7c8197f6fe
Devansh Goenka <dgoenka@wisc.edu>
2022-09-26 17:21:03 -0500

    Correct paramter in script

--- part3/run_persist.sh
+++ part3/run_persist.sh
@@ -7,19 +7,19 @@ NOCOLOR='\033[0m'
 
 # Test: Persistence Mode
 echo "${BLUE} Running Job for Partitions = 200, Persistence Mode = Memory Only ${NOCOLOR}"
-/users/hrpatel5/spark-3.3.0-bin-hadoop3/bin/spark-submit --master spark://10.10.1.1:7077 pagerank.py --partitions 200 --persistence Memory_Only
+/users/hrpatel5/spark-3.3.0-bin-hadoop3/bin/spark-submit --master spark://10.10.1.1:7077 pagerank.py --partitions 200 --persist Memory_Only
 echo "${BLUE} Job for Partitions = 200,Persistence Mode = Memory Only finished ${NOCOLOR}"
 echo "${BLUE} Removing output directory ${NOCOLOR}"
 /users/hrpatel5/hadoop-3.3.4/bin/hdfs dfs -rm -r -f /output/*
 
 echo "${BLUE} Running Job for Partitions = 200, Persistence Mode = Disk Only ${NOCOLOR}"
-/users/hrpatel5/spark-3.3.0-bin-hadoop3/bin/spark-submit --master spark://10.10.1.1:7077 pagerank.py --partitions 200 --persistence Disk_Only
+/users/hrpatel5/spark-3.3.0-bin-hadoop3/bin/spark-submit --master spark://10.10.1.1:7077 pagerank.py --partitions 200 --persist Disk_Only
 echo "${BLUE} Job for Partitions = 200,Persistence Mode = Disk Only finished ${NOCOLOR}"
 echo "${BLUE} Removing output directory ${NOCOLOR}"
 /users/hrpatel5/hadoop-3.3.4/bin/hdfs dfs -rm -r -f /output/*
 
 echo "${BLUE} Running Job for Partitions = 200, Persistence Mode = Memory_And_Disk ${NOCOLOR}"
-/users/hrpatel5/spark-3.3.0-bin-hadoop3/bin/spark-submit --master spark://10.10.1.1:7077 pagerank.py --partitions 200 --persistence Memory_And_Disk
+/users/hrpatel5/spark-3.3.0-bin-hadoop3/bin/spark-submit --master spark://10.10.1.1:7077 pagerank.py --partitions 200 --persist Memory_And_Disk
 echo "${BLUE} Job for Partitions = 200,Persistence Mode = Memory_And_Disk finished ${NOCOLOR}"
 echo "${BLUE} Removing output directory ${NOCOLOR}"
 /users/hrpatel5/hadoop-3.3.4/bin/hdfs dfs -rm -r -f /output/*
