04f1ef86c181f0f32795cad8dd0c9a1d1f0d3b0b
bbritto <bbritto@node0.assgnt1-group15.uwmadison744-f22-pg0.wisc.cloudlab.us>
2022-09-25 19:40:47 -0600

    add script for part 2

--- part2/sort.py
+++ part2/sort.py
@@ -1,20 +1,30 @@
 from pyspark.sql import SparkSession
+import argparse
+
+# Default parameters set
+APP_NAME = "SortingApp"
+
+# Parsing command line args
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", type=str, help="The path of the input file.")
+parser.add_argument("--output", type=str, help="The path of the output file.")
+args = parser.parse_args()
 
 # Create Spark context
-spark = SparkSession.builder.appName("SortingApp").getOrCreate()
-
+spark = SparkSession.builder.appName(APP_NAME).getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
 
 # Load DF into Spark
-df = spark.read.format("csv").option("header", "true").load("hdfs://10.10.1.1:9000/part1/export.csv")
-
+df = spark.read.format("csv").option("header", "true").load(args.input)
 print(f"Data read from HDFS: {df.count()} rows")
 
 # Sort the dataframe first on country code and then timestamp
 df = df.sort("cca2", "timestamp")
-
-df.write.option("header", True).csv("hdfs://10.10.1.1:9000/part1/sorted.csv")
-
+df.write.\
+   mode("overwrite").\
+   option("header", True).\
+   csv(args.output)
+   
 print(f"Data sorted and stored in HDFS: {df.count()} rows")
 
 spark.stop()
