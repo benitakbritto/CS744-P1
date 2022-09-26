578df0ee7a26b28f7fdf5c62b6657f6528c4b9f3
bbritto <bbritto@node0.assgnt1-group15.uwmadison744-f22-pg0.wisc.cloudlab.us>
2022-09-25 19:22:29 -0600

    add script for number of partitions test

--- part3/run.sh
+++ part3/run.sh
@@ -1,9 +1,24 @@
 export HDFS_NAMENODE_USER=root;
 export HDFS_DATANODE_USER=root;
 export HDFS_SECONDARYNAMENODE_USER=root;
+BLUE='\033[0;44m'
+NOCOLOR='\033[0m'
 
-/users/hrpatel5/spark-3.3.0-bin-hadoop3/bin/spark-submit --master spark://10.10.1.1:7077 pagerank.py;
+# Test: Number of partitions
+echo "${BLUE} Running Job for Partitions = 50 ${NOCOLOR}"
+/users/hrpatel5/spark-3.3.0-bin-hadoop3/bin/spark-submit --master spark://10.10.1.1:7077 pagerank.py --partitions 50
 
-echo "Run 2"
+echo "${BLUE} Running Job for Partitions = 100 ${NOCOLOR}"
+/users/hrpatel5/spark-3.3.0-bin-hadoop3/bin/spark-submit --master spark://10.10.1.1:7077 pagerank.py --partitions 100
 
-/users/hrpatel5/spark-3.3.0-bin-hadoop3/bin/spark-submit --master spark://10.10.1.1:7077 pagerank.py;
+echo "${BLUE} Running Job for Partitions = 150 ${NOCOLOR}"
+/users/hrpatel5/spark-3.3.0-bin-hadoop3/bin/spark-submit --master spark://10.10.1.1:7077 pagerank.py --partitions 150
+
+echo "${BLUE} Running Job for Partitions = 200 ${NOCOLOR}"
+/users/hrpatel5/spark-3.3.0-bin-hadoop3/bin/spark-submit --master spark://10.10.1.1:7077 pagerank.py --partitions 200
+
+echo "${BLUE} Running Job for Partitions = 250 ${NOCOLOR}"
+/users/hrpatel5/spark-3.3.0-bin-hadoop3/bin/spark-submit --master spark://10.10.1.1:7077 pagerank.py --partitions 250
+
+echo "${BLUE} Running Job for Partitions = 300 ${NOCOLOR}"
+/users/hrpatel5/spark-3.3.0-bin-hadoop3/bin/spark-submit --master spark://10.10.1.1:7077 pagerank.py --partitions 300
